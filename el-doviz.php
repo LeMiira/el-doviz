3843db44bd4272dbe840957920a23a94f39ccdd1
Mira <amindiary@gmail.com>
2026-05-25 23:51:27 +0300

    Fix plugin header: remove Update URI and match tested up to version with readme

--- el-doviz.php
+++ el-doviz.php
@@ -11,9 +11,8 @@
  * Text Domain: el-doviz
  * Domain Path: /languages
  * Requires at least: 6.0
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * Requires PHP: 7.4
- * Update URI: https://github.com/LeMiira/el-doviz
  */
 
 // 1. ABSPATH protection
